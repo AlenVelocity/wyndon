d0888dea4d5c88d1bbbc98dfa42259848a78e8d0
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-07-08 10:01:29 +0530

    update Example

--- Examples/App/index.js
+++ Examples/App/index.js
@@ -2,10 +2,27 @@ const App = require('../../dist').default
 const app = new App()
 
 app.use((req, _res, next) => {
-    console.log(`Request: ${req.url}`)
+    console.group({ request: req.url, query: req.query })
     next()
 })
+
 app.use('/well', (_req, res) => res.end('Well...'))
+
+app.use('/json', (req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    })
+    res.end(JSON.stringify({
+        string: 'Hi!',
+        number: 1,
+        boolean: true,
+        null: null,
+        array: [1, 2, 3],
+        object: {
+            key: 'value'
+        }
+    }))
+})
 
 app.use('/', (req, res) => res.end(`Hello ${req.query.name || 'There'}! Welcome to Yet another web application framework!`))
 
