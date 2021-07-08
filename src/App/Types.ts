aee1b676ca24916ba177c392792039416ee65e9d
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-07-08 11:47:53 +0530

    Update Types

--- src/App/Types.ts
+++ src/App/Types.ts
@@ -1,13 +1,8 @@
 import http from 'http'
 import IncomingMessage from './IncomingMessage'
-
+import ServerResponse from './ServerResponse'
 export type NextFunction = (err?: Error) => void
-export type SimpleHandleFunction = (req: http.IncomingMessage, res: http.ServerResponse) => void
-export type NextHandleFunction = (req: IncomingMessage, res: http.ServerResponse, next: NextFunction) => void
-export type ErrorHandleFunction = (
-    err: Error,
-    req: IncomingMessage,
-    res: http.ServerResponse,
-    next: NextFunction
-) => void
+export type SimpleHandleFunction = (req: http.IncomingMessage, res: ServerResponse) => void
+export type NextHandleFunction = (req: IncomingMessage, res: ServerResponse, next: NextFunction) => void
+export type ErrorHandleFunction = (err: Error, req: IncomingMessage, res: ServerResponse, next: NextFunction) => void
 export type HandleFunction = SimpleHandleFunction | NextHandleFunction | ErrorHandleFunction
