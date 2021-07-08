335489383079163296e29eed1306785d529be407
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-07-08 17:08:07 +0530

    Update Example

--- Examples/App/index.js
+++ Examples/App/index.js
@@ -47,6 +47,4 @@ app.use('/json', (req, res) => {
 
 app.use('/', (req, res) => res.end(`Hello ${req.query.name || 'There'}! Welcome to Yet another web application framework!`))
 
-app.listen(5000)
-
-console.log('App is listening on post 5000')
+app.listen(5000, () => console.log('App is listening on post 5000'))
