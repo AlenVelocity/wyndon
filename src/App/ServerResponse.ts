95baf93b3b9c42bfbdd4efe5026a95c442ac7da5
Alen Yohannan <66544823+AlenSaito1@users.noreply.github.com>
2021-07-09 18:05:21 +0530

    Make the `render` method a required method in `ServerResponse`

--- src/App/ServerResponse.ts
+++ src/App/ServerResponse.ts
@@ -1,5 +1,5 @@
 import http from 'http'
 export default interface ServerResponse extends http.ServerResponse {
     json(data: unknown): void
-    render?(filename?: string, options?: { [key: string]: unknown }, callback?: () => unknown): void
+    render(filename?: string, options?: { [key: string]: unknown }, callback?: () => unknown): void
 }
